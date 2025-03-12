34c9c8268ca0658366013ce5896d8ea5edabff72
Sushanta Bhandari <89909878+Areszyn@users.noreply.github.com>
2025-03-13 02:02:59 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import Head from "next/head";
 import "./globals.css";
 import { Nav } from "@/components/Nav";
 import { cn } from "@/utils";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Ai Voice",
   description: "A Next.js starter using Hume AI's Empathic Voice Interface",
+  other: {
+    monetag: "6c390819b6af56a65b7afa261d811540", // This will generate <meta name="monetag" content="..." />
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="monetag" content="6c390819b6af56a65b7afa261d811540">
-      </Head>
       <body className={cn(GeistSans.variable, GeistMono.variable, "flex flex-col min-h-screen")}>
         <Nav />
         {children}
