93fcea079d7508492c39a6e799e25fb46c57c850
Sushanta Bhandari <89909878+Areszyn@users.noreply.github.com>
2025-03-13 01:11:35 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,22 @@
-import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+import Head from "next/head";
 import "./globals.css";
 import { Nav } from "@/components/Nav";
 import { cn } from "@/utils";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Ai Voice",
   description: "A Next.js starter using Hume AI's Empathic Voice Interface",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={cn(
-          GeistSans.variable,
-          GeistMono.variable,
-          "flex flex-col min-h-screen"
-        )}
-      >
+      <Head>
+        <meta name="monetag" content="6c390819b6af56a65b7afa261d811540" />
+      </Head>
+      <body className={cn(GeistSans.variable, GeistMono.variable, "flex flex-col min-h-screen")}>
         <Nav />
         {children}
       </body>
