9ecd8d53fcda97ac237bc5a9d27d4ecaf530c45f
Areszyn Waspros <89909878+Areszyn@users.noreply.github.com>
2025-04-19 13:17:26 +0545

    Update Hume.tsx

--- components/logos/Hume.tsx
+++ components/logos/Hume.tsx
@@ -2,14 +2,16 @@ import type { FC, ImgHTMLAttributes } from "react";
 
 export type WasprosLogoProps = ImgHTMLAttributes<HTMLImageElement>;
 
-export default function WasprosLogo(props: WasprosLogoProps) {
+const WasprosLogo: FC<WasprosLogoProps> = (props) => {
   return (
     <img
-      src="/waspros.png" // Ensure waspros.png is in the public folder
+      src="https://envs.sh/YTD.jpg"
       alt="Waspros Logo"
       width="106"
       height="25"
       {...props}
     />
   );
-}
+};
+
+export default WasprosLogo;
